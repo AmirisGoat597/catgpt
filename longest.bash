d1f9d274f74abf66d7f8f435f1f22eab969b97eb
AmirisGoat597 <Amir.Mahbouby.1@gmail.com>
2026-06-30 09:48:04 +0800

    Change MY_API_KEY in longest.bash
    Updated MY_API_KEY to a new value.

--- longest.bash
+++ longest.bash
@@ -1,4 +1,5 @@
-export MY_API_KEY="fish_f572a80be57c61a26a4f5875b4a9040781abc59cb0c183285babd656b701aeb5"
+export MY_API_KEY="AQ.Ab8RN6JeXqjAGRlkQ5hGShcR1HOF92ZMOlnyKS3dVtNa9y-Szg
+"
 
 curl https://amirisgoat597.github.io/catgpt/ \
   -H "Authorization: Bearer $MY_API_KEY" \
